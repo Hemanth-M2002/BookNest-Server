28df4dadc1e2154070f437e0ed2ce84610d88012
Hemanth-M2002 <hemanthmathivanan02@gmail.com>
2024-10-06 14:24:12 +0530

    final 1

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors"); 
 const userRoutes = require("./routes/bookRoutes");
+const allowedOrigins = ['https://book-nest-chi.vercel.app', 'http://localhost:3000'];
+
 
 const app = express();
 const PORT = process.env.PORT || 7001;
 
-app.use(cors()); 
-app.use(
-  cors({
-    origin: "http://localhost:3000", 
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 app.use(express.json());
 //mongodb connection
