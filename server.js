b665e25233ad7f76a2262cf68c112f2264608ad4
Gururaj <guruguru6631@gmail.com>
2024-10-06 11:49:50 +0530

    server-deploy

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors"); 
+const userRoutes = require("./routes/bookRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 7001;
+
+app.use(cors()); 
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+  })
+);
+
+app.use(express.json());
+//mongodb connection
+mongoose.connect(
+  "mongodb+srv://hemanthvijay02:XQMwVhpa40X2cFQ5@book-cluster.sdxv3.mongodb.net/booknest",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use("/api", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
